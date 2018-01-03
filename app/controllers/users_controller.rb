d681ba9e685cff0c7fa08561e2b87bcdf607165a
DoughBoy79 <andew.hall286@gmail.com>
2018-01-03 05:45:24 +0000

    user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+    
+    #Creates the new user object
+    def new
+       @user = User.new 
+    end
+    
+    #Makes sure that the user information entered in the new view fits in the model and if so adds the new data to the model
+    def create
+        @user = User.new(params.require(:user).permit(:first_name,:last_name,:email,password_digest,password_confirmation))
+        if @user.save
+            
+        else
+            render 'new' 
+        end
+    end
+    
+    def destroy
+        @user = User.find(params([:id]))
+        @user.destroy
+    end
+    
+    def index
+        @user = User.all
+    end
+    
+    def show
+        @user = User.find(params([:id]))
+    end
+end

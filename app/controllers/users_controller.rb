6e87aab368497554ef0bb0ac71432d592391db51
DoughBoy79 <andew.hall286@gmail.com>
2018-01-09 02:11:37 +0000

    newpages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,11 @@ class UsersController < ApplicationController
     
     #Makes sure that the user information entered in the new view fits in the model and if so adds the new data to the model
     def create
-        @user = User.new(params.require(:user).permit(:first_name,:last_name,:email,password,password_confirmation))
+        @user = User.new(params.require(:user).permit(:first_name,:last_name,:email,:password,:password_confirmation))
         if @user.save
-            
+            redirect_to '/mainpage'
         else
-            render 'new' 
+            redirect_to '/signup' 
         end
     end
     
